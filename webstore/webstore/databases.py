66ca1b0257bb23bb6399cb05e895e4fbe6f94148
Alberto Jordane adolfo <albertoadolfo27@gmail.com>
2021-11-13 12:57:56 +0200

    Add databases.py files, that contains mysql and sqlite3 connections  settings

---
+++ webstore/webstore/databases.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SQLITE3 = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
+}
