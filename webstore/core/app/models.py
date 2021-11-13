9fb856aec9c71d14f08e0610a5f822f619efe56d
Alberto Jordane adolfo <albertoadolfo27@gmail.com>
2021-11-13 20:06:37 +0200

    Add models

--- webstore/core/app/models.py
+++ webstore/core/app/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(max_length=255)
+    cite = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class Contact(models.Model):
+    phone_code = models.DateTimeField()
+    phone_number = models.DateTimeField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class Person(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    birthday = models.DateTimeField()
+    gender = models.CharField(max_length=45)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, null=True)
+
+
+class Type(models.Model):
+    id = models.PositiveIntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    state = models.PositiveIntegerField(default=1)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    person = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+
+
+class Cart(models.Model):
+    session_id = models.CharField(max_length=255)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class ProductCart(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+
+
+class Request(models.Model):
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_quantity = models.PositiveIntegerField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+
+
+class Payment(models.Model):
+    state = models.PositiveIntegerField(default=1)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    request = models.ForeignKey(Request, on_delete=models.SET_NULL, null=True)
